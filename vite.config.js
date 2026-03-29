f5c2b1fc2609df297170e0bcbdfa2ad3d2023c1d
Muhammad <muhammadzaidfida@gmail.com>
2026-03-29 18:28:43 +0500

    Fix: Re-add cache buster glowere-bot.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         // Force fixed names for embedding
-        entryFileNames: `assets/main.js`,
+        entryFileNames: `assets/glowere-bot.js`,
         chunkFileNames: `assets/main.js`,
         assetFileNames: `assets/[name].[ext]`,
       },
